e1a1b87d1e24e26c8a695e8736b0a103a5425660
DarkXerXerS <darkxerxers29@gmail.com>
2023-07-05 17:05:50 -0700

    delected some lines of code

--- node.js
+++ node.js
@@ -7,7 +7,7 @@ console.log(PI)                            // 3.141592653589793
 
 console.log(Math.round(PI))                // 3 to round values to the nearest number
 
-console.log(Math.round(9.81))              // 10
+console.log(Math.round(9          // 10
 
 console.log(Math.floor(PI))                // 3 rounding down
 
